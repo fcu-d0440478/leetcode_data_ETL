a064bf3ec457ded256b2b1591b8495147d6311e1
Eric <yungyi.cool@gmail.com>
2025-04-04 23:01:50 +0800

    新增環境變數載入功能並更新.gitignore以排除.env檔案

--- Load/Neon_upload_data.py
+++ Load/Neon_upload_data.py
@@ -1,11 +1,15 @@
+from dotenv import load_dotenv
+import os
 import pandas as pd
 from sqlalchemy import create_engine
 
 # 讀取你的CSV資料
 df = pd.read_csv("Leetcode 各題目基本資料表 20210712.csv")
 
-user_name = ""
-password = ""
+load_dotenv()
+
+user_name = os.getenv("USER_NAME")
+password = os.getenv("PASSWORD")
 
 # Neon 提供的資料庫連線字串
 DATABASE_URL = f"postgresql://{user_name}:{password}@ep-rapid-recipe-a1d6k3o2-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
